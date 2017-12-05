3508bb948a65a8686b4a000359f90bbb8536d796
Aythami <aythamilopezdeniz@hotmail.com>
2017-12-05 12:17:22 +0000

    Refactoring Extract Method

--- src/ContarElementosRepetidos.java
+++ src/ContarElementosRepetidos.java
@@ -1,39 +1,37 @@
 public class ContarElementosRepetidos{
-    static int A=10;
-    static int B=20;
-    static int vectorA[]=new int[A];
-    static int vectorB[]=new int[B];
-    static int elemA=0;
-    static int elemB=0;
-    static int z=0;
+    private static int A=10;
+    private static int B=20;
+    private static int vectorA[]=new int[A];
+    private static int vectorB[]=new int[B];
+    private static int elemA=0;
 
-    public static void compara(){
-        for(int i=0;i<vectorA.length;i++){
-            for(int j=0;j<vectorB.length;j++){
-                if(vectorA[i]==vectorB[j])
+    private static void generarVector(int[] Vector) {
+        for (int i = 0; i < Vector.length; i++)
+            Vector[i] = (int) (Math.random() * 100 + 1);
+    }
+
+    private static void imprimir(int[] Vector) {
+        for (int i = 0; i < Vector.length; i++) {
+            System.out.print("Vector ["+i+"]= "+Vector[i]+"\n");
+        }
+    }
+
+    private static void compara(){
+        for (int aVectorA : vectorA) {
+            for (int aVectorB : vectorB) {
+                if (aVectorA == aVectorB)
                     elemA++;
             }
-            System.out.println("El elemento "+vectorA[i]+" se presenta "+elemA+" veces en B");
-            elemA=0;
+            System.out.println("El elemento " + aVectorA + " se presenta " + elemA + " veces en B");
+            elemA = 0;
         }
     }
 
     public static void main(String[] args){
-        for(int i=0;i<vectorA.length;i++){
-            vectorA[i]=(int)(Math.random()*100+1);
-        }
-
-        for(int i=0;i<vectorB.length;i++){
-            vectorB[i]=(int)(Math.random()*100+1);
-        }
-
-        for(int i=0;i<vectorA.length;i++){
-            System.out.print("A"+"["+i+"]= "+vectorA[i]+"\n");
-        }
-
-        for(int i=0;i<vectorB.length;i++){
-            System.out.print("B"+"["+i+"]="+vectorB[i]+"\n");
-        }
+        generarVector(vectorA);
+        generarVector(vectorB);
+        imprimir(vectorA);
+        imprimir(vectorB);
         compara();
     }
 }

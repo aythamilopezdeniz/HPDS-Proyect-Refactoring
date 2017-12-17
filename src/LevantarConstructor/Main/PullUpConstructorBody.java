8461c6d7391471d24f4071d3910f1d103ca20633
Aythami <aythamilopezdeniz@hotmail.com>
2017-12-17 19:51:06 +0000

    Pull Up Constructor Body

--- src/LevantarConstructor/Main/PullUpConstructorBody.java
+++ src/LevantarConstructor/Main/PullUpConstructorBody.java
@@ -8,46 +8,59 @@ import LevantarConstructor.Seleccion.SeleccionFutbol;
 import java.util.ArrayList;
 
 public class PullUpConstructorBody {
-    // ArrayList de objetos SeleccionFutbol. Idenpendientemente de la clase hija a la que pertenezca el objeto
     private static ArrayList<SeleccionFutbol> integrantes = new ArrayList<>();
+    private static Entrenador delBosque;
+    private static Futbolista iniesta;
+    private static Masajista raulMartinez;
 
     public static void main(String[] args) {
 
-        Entrenador delBosque = new Entrenador(1, "Vicente", "Del Bosque", 60, "284EZ89");
-        Futbolista iniesta = new Futbolista(2, "Andres", "Iniesta", 29, 6, "Interior Derecho");
-        Masajista raulMartinez = new Masajista(3, "Raúl", "Martinez", 41, "Licenciado en Fisioterapia", 18);
+        delBosque = new Entrenador(1, "Vicente", "Del Bosque", 60, "284EZ89");
+        iniesta = new Futbolista(2, "Andres", "Iniesta", 29, 6, "Interior Derecho");
+        raulMartinez = new Masajista(3, "Raúl", "Martinez", 41, "Licenciado en Fisioterapia", 18);
 
         integrantes.add(delBosque);
         integrantes.add(iniesta);
         integrantes.add(raulMartinez);
 
-        // CONCENTRACION
+        concentracion();
+        viaje();
+        entrenamiento();
+        masaje();
+        partido();
+    }
+
+    private static void concentracion() {
         System.out.println("Todos los integrantes comienzan una concentracion. (Todos ejecutan el mismo método)");
         for (SeleccionFutbol integrante : integrantes) {
             System.out.print(integrante.getNombre()+" "+integrante.getApellidos()+" -> ");
             integrante.Concentrarse();
         }
+    }
 
-        // VIAJE
+    private static void viaje() {
         System.out.println("\nTodos los integrantes viajan para jugar un partido. (Todos ejecutan el mismo método)");
         for (SeleccionFutbol integrante : integrantes) {
             System.out.print(integrante.getNombre()+" "+integrante.getApellidos()+" -> ");
             integrante.Viajar();
         }
+    }
 
-        // ENTRENAMIENTO
+    private static void entrenamiento() {
         System.out.println("\nEntrenamiento: Solamente el entrenador y el futbolista tiene metodos para entrenar:");
         System.out.print(delBosque.getNombre()+" "+delBosque.getApellidos()+" -> ");
         delBosque.dirigirEntrenamiento();
         System.out.print(iniesta.getNombre()+" "+iniesta.getApellidos()+" -> ");
         iniesta.entrenar();
+    }
 
-        // MASAJE
+    private static void masaje() {
         System.out.println("\nMasaje: Solo el masajista tiene el método para dar un masaje:");
         System.out.print(raulMartinez.getNombre()+" "+raulMartinez.getApellidos()+" -> ");
         raulMartinez.darMasaje();
+    }
 
-        // PARTIDO DE FUTBOL
+    private static void partido() {
         System.out.println("\nPartido de Fútbol: Solamente el entrenador y el futbolista tiene metodos para el partido de fútbol:");
         System.out.print(delBosque.getNombre()+" "+delBosque.getApellidos()+" -> ");
         delBosque.dirigirPartido();
